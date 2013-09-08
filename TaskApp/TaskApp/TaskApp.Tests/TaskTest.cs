9f3d90717c826807ac0c7da37811d10559dd3be8
leo-guinan <leo.guinan@gmail.com>
2013-09-08 15:44:37 -0400

    Adding TastTest unit test

---
+++ TaskApp/TaskApp/TaskApp.Tests/TaskTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskApp.Domain;
+
+namespace TaskApp.Tests
+{
+    [TestClass]
+    public class TaskTest
+    {
+        private Task task = new Task();
+
+        [TestMethod]
+        public void testValidTask()
+        {
+            task.name = "taskName";
+                
+            task.estimateId = 1;
+            task.id = 1;
+            Assert.IsTrue(task.validateTask());
+        }
+
+        [TestMethod]
+        public void testInvalidTaskNullName()
+        {
+            task.name = null;
+
+            task.estimateId = 1;
+            task.id = 1;
+            Assert.IsFalse(task.validateTask());
+        }
+
+     
+    }
+}
