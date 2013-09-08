1805951ca1c3af90d5eea94ad4c1c92de961db0b
leo-guinan <leo.guinan@gmail.com>
2013-09-08 15:45:48 -0400

    Adding Database Creation Scripts

---
+++ Database Scripts/CreateEstimateTable.sql
@@ -0,0 +1,28 @@
+USE [tasksDB]
+GO
+
+/****** Object:  Table [dbo].[Estimate]    Script Date: 9/8/2013 1:01:59 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Estimate](
+	[id] [int] NOT NULL,
+	[time] [int] NOT NULL,
+	[type] [varchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
